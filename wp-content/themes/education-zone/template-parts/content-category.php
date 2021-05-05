398582c8859eee18cba6941a49ecf5c0b4a40d7a
Phuc Nguyen <nhphuc@starboardasia.com>
2021-05-05 15:38:28 +0700

    update category

--- wp-content/themes/education-zone/template-parts/content-category.php
+++ wp-content/themes/education-zone/template-parts/content-category.php
@@ -6,9 +6,22 @@
  *
  * @package Education_Zone
  */
+$term = get_queried_object();
+$categories = get_categories(
+    array( 
+    	'parent' => $term->term_id,
+    	'orderby' => 'id',
+    	'order'   => 'ASC'
+    )
+);
+$is_parent = true;
+if (empty($categories)){
+	$categories[0] = $term;
+	$is_parent = false;
+}
 ?>
 <section class="category-product">
-    <h1 class="title uk-text-left">Drilling &amp; Fastening</h1>
+    <h1 class="title uk-text-left"><?php echo single_cat_title( '', false );?></h1>
     <div class="bnm-category-compare listCompare">
 		<div class="list-items-compare">
 			<div class="row">
@@ -40,24 +53,48 @@
 			</div>
 		</div>	
     </div>
-	<div class="items-by-category">
-		<h2 class="title uk-text-left">Hammer Driver Drills </h2>
+	<?php
+	    if ( $categories ) {
+	    	$i = 0;
+            foreach ( $categories as $category ) :
+
+	?>    
+	<div class="items-by-category<?php echo $i > 0 ? ' pt40' : ''?>">
+		<?php if ($is_parent): ?>
+		<h2 class="title uk-text-left"><?php echo $category->name;?></h2>
+	    <?php endif;?>
 		<div class="items">
 			<div class="row">
+				<?php
+					$products = get_posts( array(
+					    'post_type'      => 'product',
+					    'posts_per_page' => -1,
+					    'post_status'    => 'publish',
+					    'category'       => $category->cat_ID,
+					    'orderby' => 'id',
+    	                'order'   => 'ASC'
+					) );
+				?>
+				<?php
+				    if ( $products ) {
+                        foreach ( $products as $post ) :
+                        	$post_id = get_the_ID();                        	
+                        	$product_code = get_field_object('product_code' , $post_id);
+				?>
 				<div class="col-md-3 col-sm-6 col-xs-12">
 					<div class="panel-item">
 						<div class="thumnail-item"> 
-							<a href="/building-construction/category/drilling-fastening/hammer-driver-drills/hp001gz-40v-max-brushless-hammer-driver-drill" class="MagicZoom-single">
-							<img src="http://dev.makita.com/wp-content/uploads/2021/05/hp001gz-001_f47ac4b93444b2c313208a8dcfa14207.jpg">
+							<a href="<?php the_permalink(); ?>" class="MagicZoom-single">
+							<img src="<?php echo get_the_post_thumbnail_url($post_id,'full');?>">
 							</a>
 						</div>
 						<div class="bnm-item-title uk-flex-item-1">
 							<h3 class="uk-margin-remove">
-								<a title="HP001GZ- 40V Max Brushless Hammer Driver Drill" href="/building-construction/category/drilling-fastening/hammer-driver-drills/hp001gz-40v-max-brushless-hammer-driver-drill">HP001GZ- 40V Max Brushless Hammer Driver Drill</a> 
+								<a title="<?php the_title(); ?>" href="<?php the_permalink(); ?>"><?php the_title(); ?></a> 
 							</h3>
 						</div>
 						<div class="bnm-compare-element">
-							<span class="zoocompare-element bnm-zoocompare zl-bootstrap" data-item-id="1" data-item-data="">
+							<span class="zoocompare-element bnm-zoocompare zl-bootstrap" data-item-id="<?php echo $post_id; ?>" data-item-code="<?php echo empty($product_code['value']) ?  '' : $product_code['value'];?>">
 								<span> <input type="checkbox" name="zoocompare" class="zoocompareCheckbox"> </span>
 								<a class="bnmzoocompare uk-button uk-button-small uk-button-primary" title="Compare this product" href="javascript:void(0)">
 								<span class="zoocompare compare">Compare</span>
@@ -66,102 +103,18 @@
 						</div>
 					</div>
 				</div>
-
-				<div class="col-md-3 col-sm-6 col-xs-12">
-					<div class="panel-item">
-						<div class="thumnail-item"> 
-							<a href="/building-construction/category/drilling-fastening/hammer-driver-drills/hp001gz-40v-max-brushless-hammer-driver-drill" class="MagicZoom-single">
-							<img src="http://dev.makita.com/wp-content/uploads/2021/05/hp001gz-001_f47ac4b93444b2c313208a8dcfa14207.jpg">
-							</a>
-						</div>
-						<div class="bnm-item-title uk-flex-item-1">
-							<h3 class="uk-margin-remove">
-								<a title="HP001GZ- 40V Max Brushless Hammer Driver Drill" href="/building-construction/category/drilling-fastening/hammer-driver-drills/hp001gz-40v-max-brushless-hammer-driver-drill">DHP481Z-18V Mobile Brushless Heavy Duty Hammer Driver Drill</a> 
-							</h3>
-						</div>
-						<div class="bnm-compare-element">
-							<span class="zoocompare-element bnm-zoocompare zl-bootstrap" data-item-id="2" data-item-data="">
-								<span> <input type="checkbox" name="zoocompare" class="zoocompareCheckbox"> </span>
-								<a class="bnmzoocompare uk-button uk-button-small uk-button-primary" title="Compare this product" href="javascript:void(0)">
-								<span class="zoocompare compare">Compare</span>
-								</a>
-							</span>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-md-3 col-sm-6 col-xs-12">
-					<div class="panel-item">
-						<div class="thumnail-item"> 
-							<a href="/building-construction/category/drilling-fastening/hammer-driver-drills/hp001gz-40v-max-brushless-hammer-driver-drill" class="MagicZoom-single">
-							<img src="http://dev.makita.com/wp-content/uploads/2021/05/hp001gz-001_f47ac4b93444b2c313208a8dcfa14207.jpg">
-							</a>
-						</div>
-						<div class="bnm-item-title uk-flex-item-1">
-							<h3 class="uk-margin-remove">
-								<a title="HP001GZ- 40V Max Brushless Hammer Driver Drill" href="/building-construction/category/drilling-fastening/hammer-driver-drills/hp001gz-40v-max-brushless-hammer-driver-drill">DHP484Z-18V Mobile Brushless Heavy Duty Compact Hammer Driver Drill</a> 
-							</h3>
-						</div>
-						<div class="bnm-compare-element">
-							<span class="zoocompare-element bnm-zoocompare zl-bootstrap" data-item-id="3" data-item-data="">
-								<span> <input type="checkbox" name="zoocompare" class="zoocompareCheckbox"> </span>
-								<a class="bnmzoocompare uk-button uk-button-small uk-button-primary" title="Compare this product" href="javascript:void(0)">
-								<span class="zoocompare compare">Compare</span>
-								</a>
-							</span>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-md-3 col-sm-6 col-xs-12">
-					<div class="panel-item">
-						<div class="thumnail-item"> 
-							<a href="/building-construction/category/drilling-fastening/hammer-driver-drills/hp001gz-40v-max-brushless-hammer-driver-drill" class="MagicZoom-single">
-							<img src="http://dev.makita.com/wp-content/uploads/2021/05/hp001gz-001_f47ac4b93444b2c313208a8dcfa14207.jpg">
-							</a>
-						</div>
-						<div class="bnm-item-title uk-flex-item-1">
-							<h3 class="uk-margin-remove">
-								<a title="HP001GZ- 40V Max Brushless Hammer Driver Drill" href="/building-construction/category/drilling-fastening/hammer-driver-drills/hp001gz-40v-max-brushless-hammer-driver-drill">HP333DZ- 12V Max Hammer Driver Drill</a> 
-							</h3>
-						</div>
-						<div class="bnm-compare-element">
-							<span class="zoocompare-element bnm-zoocompare zl-bootstrap" data-item-id="4" data-item-data="">
-								<span> <input type="checkbox" name="zoocompare" class="zoocompareCheckbox"> </span>
-								<a class="bnmzoocompare uk-button uk-button-small uk-button-primary" title="Compare this product" href="javascript:void(0)">
-								<span class="zoocompare compare">Compare</span>
-								</a>
-							</span>
-						</div>
-					</div>
-				</div>	
-
-				<div class="col-md-3 col-sm-6 col-xs-12">
-					<div class="panel-item">
-						<div class="thumnail-item"> 
-							<a href="/building-construction/category/drilling-fastening/hammer-driver-drills/hp001gz-40v-max-brushless-hammer-driver-drill" class="MagicZoom-single">
-							<img src="http://dev.makita.com/wp-content/uploads/2021/05/hp001gz-001_f47ac4b93444b2c313208a8dcfa14207.jpg">
-							</a>
-						</div>
-						<div class="bnm-item-title uk-flex-item-1">
-							<h3 class="uk-margin-remove">
-								<a title="HP001GZ- 40V Max Brushless Hammer Driver Drill" href="/building-construction/category/drilling-fastening/hammer-driver-drills/hp001gz-40v-max-brushless-hammer-driver-drill">HP333DZ- 12V Max Hammer Driver Drill</a> 
-							</h3>
-						</div>
-						<div class="bnm-compare-element">
-							<span class="zoocompare-element bnm-zoocompare zl-bootstrap" data-item-id="5" data-item-data="">
-								<span> <input type="checkbox" name="zoocompare" class="zoocompareCheckbox"> </span>
-								<a class="bnmzoocompare uk-button uk-button-small uk-button-primary" title="Compare this product" href="javascript:void(0)">
-								<span class="zoocompare compare">Compare</span>
-								</a>
-							</span>
-						</div>
-					</div>
-				</div>							
-
+				<?php
+				        endforeach; 
+				    }
+				?>					
 			</div>
 		</div>	
-	</div>    
+	</div>
+	<?php
+	        $i++;
+	        endforeach; 
+	    }
+	?>   	
 </section>
 
 <script>
@@ -169,8 +122,9 @@
 		jQuery('.zoocompareCheckbox').change(function() {
 			var isCheck = jQuery(this).is(':checked');
 			var item_id = jQuery(this).parent().parent().data("item-id");
+			var item_code = jQuery(this).parent().parent().data("item-code");
 			if (isCheck){
-				var html = '<div class="jk-inner jk-compare-inner" data-item-id="'+ item_id +'" data-item=""><p class="jk-product"><span class="product-code">DF001GZ</span><span onclick="removeCompareModule(this);" class="jk-remove-compare icon-remove" data-item-id="'+ item_id +'" data-item-data=""><i class="fas fa-times"></i</span></p></div>';
+				var html = '<div class="jk-inner jk-compare-inner" data-item-id="'+ item_id +'"><p class="jk-product"><span class="product-code">'+ item_code +'</span><span onclick="removeCompareModule(this);" class="jk-remove-compare icon-remove" data-item-id="'+ item_id +'" data-item-code="'+ item_code +'"><i class="fas fa-times"></i</span></p></div>';
 				jQuery(".items-compare .jk-item").each(function () {
 	                if (jQuery(this).html().trim().length < 5) {
 	                    jQuery(this).append(html);
@@ -210,15 +164,22 @@
 	
 	function jkCompareLink(){
 	    var link = false;
+	    var i = 0;
 	    jQuery('.items-compare .jk-item .jk-inner').each(function(){
 	        if(jQuery(this).length > 0){
-	            var data = jQuery(this).data('item');
-	            link = '?controller=compare&task=compare&app_id='+data.app_id+'&type='+data.type;
+	            var item_id = jQuery(this).data('item-id');
+	            if (i == 0){
+	            	link = '/compare?item_id['+i+']='+ item_id ;
+	            }else{
+	            	link += '&item_id['+i+']='+ item_id ;
+	            }
+	            
+	            i++;
 	            //break;
 	        }
 	    });
 	    if(link){
-	        window.location.href= 'https://www.makita.com.au/building-construction/category/drilling-fastening' + link;
+	        window.location.href= '<?php echo site_url();?>' + link;
 	    }else alert('Please select product to compare');
 	}
 	
---
+++ wp-content/themes/education-zone/templates/template-compare.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ Template Name: Compare Page
+ */
+
+get_header();
+$lg = get_locale();
+?>
+<?php
+get_footer();
